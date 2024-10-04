89cb1b0afa77b792ebea9b87ffda4c7b7b3a1bd0
Jag Jeevan Ricky <avengr7890@gmail.com>
2024-10-04 16:19:54 +0530

    #

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Welcome = () => {
   return (
-    <div className="w-full h-dvh bg-zinc-900 flex justify-center items-center">
-      <nav>
-          <h1 className="text-3xl text-white">Welcome to my website</h1>
+    <div className="w-full h-dvh bg-zinc-900">
+      <nav className="w-full p-4 bg-white">
+          <h1 className="text-3xl">Tiwari BKL</h1>
         </nav>
         <div className="h-2/4 w-96 bg-white rounded-lg">
           <h1 className="flex justify-center p-4 text-lg font-bold">Welcome Page</h1>
