ee085d630d5d51026f117015b411eeb85c2ef080
Jag Jeevan Ricky <avengr7890@gmail.com>
2024-10-04 16:03:16 +0530

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-import SignUp from './Components/User/signup'
+import Welcome from './Components/Welcome'
 
 function App() {
 
   return (
     <>
-      <SignUp /> 
+      <Welcome /> 
     </>
   )
 }
