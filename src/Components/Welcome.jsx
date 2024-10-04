86bcbc69aa741f3208e5028271b1f35eaf1f6b2d
Jag Jeevan Ricky <avengr7890@gmail.com>
2024-10-04 16:33:35 +0530

    #

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Welcome = () => {
   return (
     <div className="w-full h-dvh bg-zinc-900 flex flex-col items-center">
-      <nav className="w-full p-4  bg-white flex justify-between items-center ">
+      <nav className="w-full p-4 px-16 bg-white flex justify-between items-center ">
           <h1 className="text-3xl">Tiwari BKL</h1>
           <div className="flex gap-8">
             <h1 className="cursor-pointer hover:text-red-500">Home</h1>
@@ -14,8 +14,10 @@ const Welcome = () => {
             <h1 className="cursor-pointer hover:text-red-500">SignUp</h1>
           </div>
         </nav>
-        <div className="h-2/4 w-96 bg-white rounded-lg flex justify-center items-center">
-          <h1 className="flex justify-center p-4 text-lg font-bold">Welcome Page</h1>
+        <div className="text-white flex flex-col justify-center items-center h-full w-11/12">
+          <h1 className="flex justify-center p-4 text-6xl font-extrabold">Welcome To the Jungle</h1>
+          <h1 className="flex justify-center p-4 text-lg font-bold">Tiwari ka Pakoda yeha milta hai.</h1>
+          <p className="text-center px-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi iure nihil, aspernatur eveniet laudantium cum a tenetur ea porro in distinctio sunt ipsum quae. Facilis recusandae harum doloribus provident? Facere placeat fuga, quae amet earum eligendi corporis perspiciatis, dignissimos autem unde, nostrum sit temporibus aut.</p>
       </div>
     </div>
   );
